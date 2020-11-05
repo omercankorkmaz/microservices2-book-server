cdeb79ea6dcfc9b1bd29acc7c96b05dba5c56bcd
Ömer Can Korkmaz <omercankorkmaz0@gmail.com>
2020-11-05 09:43:25 +0300

    INITIAL

---
+++ src/main/java/com/bookserver/Microservices2BookServerApplication.java
@@ -0,0 +1,13 @@
+package com.bookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservices2BookServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservices2BookServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookserver/Microservices2BookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservices2BookServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
